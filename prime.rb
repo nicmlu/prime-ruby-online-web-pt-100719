7ab7226078e2f608471bdf511d09c13aae2ab557
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-23 19:09:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+# input = integer 
+# output = return boolean t/f
+# determine if prime (prime number= num can only be divided by 1 and itself)
+
+def prime?(int)
+  if int/int == 0 && int/1 == int 
+    puts true
+  else 
+    puts false
+  end
+end
