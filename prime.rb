fde8b6443d5704df93f48d8d1a86dac12e565031
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-24 02:26:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,20 @@
 # determine if prime (prime number= num can only be divided by 1 and itself)
 
 def prime?(int)
-  if int >= 2 
-    
-    int/int == 0 && int/1 == int 
-    true
+  if int >= 2
+    # why create a range of #'s ~> testing to see if int is diviable by any number in the range
+    # range = (2...int).to_a
+
+    # all will return true when empty []
+    # using implicit block which renders true
+    # range.all? do |num|
+    #   int % num != 0
+    # end
+
+    # range.all? {|num| int % num != 0 }
+
+    (2...int).all? {|num| int % num != 0 }
+  
   else 
     false
   end
